ab31779e9dea384c03237f197eb62d549ddc36f7
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2024-11-07 18:13:21 +0200

    feat: update server main

--- kaonic/src/main.cpp
+++ kaonic/src/main.cpp
@@ -2,16 +2,30 @@
 
 #include <memory>
 
+#include "kaonic/comm/common/logging.hpp"
+#include "kaonic/comm/radio/rf215_radio.hpp"
+#include "kaonic/grpc/context.hpp"
 #include "kaonic/grpc/device_service.hpp"
 #include "kaonic/grpc/radio_service.hpp"
+
+#include "version.hpp"
 
 #include <grpcpp/server_builder.h>
 
 using namespace kaonic;
 
 auto main(int argc, char** argv) noexcept -> int {
-    kaonic::grpc::device_service device_serv;
-    kaonic::grpc::radio_service radio_serv;
+    log::info("[Server] Start comm server with FW version {}", VERSION);
+
+    std::unique_ptr<comm::radio> radio = std::make_unique<comm::rf215_radio>();
+    if (auto err = radio->open(); !err.is_ok()) {
+        log::error("[Server] Unable to open radio device: {}", err.to_str());
+        return -1;
+    }
+
+    auto context = std::make_shared<kaonic::grpc::context>(std::move(radio), VERSION);
+    kaonic::grpc::device_service device_serv(context);
+    kaonic::grpc::radio_service radio_serv(context);
 
     ::grpc::ServerBuilder builder;
     builder.AddListeningPort("0.0.0.0:50051", ::grpc::InsecureServerCredentials());
@@ -19,6 +33,8 @@ auto main(int argc, char** argv) noexcept -> int {
     builder.RegisterService(&radio_serv);
 
     std::unique_ptr<::grpc::Server> server(builder.BuildAndStart());
+    log::info("[Server] Server is listening on port 50051");
+
     server->Wait();
 
     return 0;

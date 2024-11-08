2e1786d7bc2077f0c18f146c99d7afe95af70048
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2024-11-08 15:49:36 +0200

    style: change device_service class style

--- kaonic/include/kaonic/grpc/device_service.hpp
+++ kaonic/include/kaonic/grpc/device_service.hpp
@@ -12,6 +12,7 @@ class device_service final : public Device::Service {
 
 public:
     explicit device_service(const std::shared_ptr<context>& context);
+    ~device_service() = default;
 
     [[nodiscard]] auto GetInfo(::grpc::ServerContext* context,
                                const Empty* request,
@@ -22,14 +23,14 @@ public:
                                      StatisticsResponse* response) -> ::grpc::Status final;
 
 protected:
-    device_service(const device_service&) = default;
-    device_service(device_service&&) = default;
+    device_service(const device_service&) = delete;
+    device_service(device_service&&) noexcept = delete;
 
-    device_service& operator=(const device_service&) = default;
-    device_service& operator=(device_service&&) = default;
+    device_service& operator=(const device_service&) = delete;
+    device_service& operator=(device_service&&) noexcept = delete;
 
 private:
-    std::shared_ptr<context> _context = nullptr;
+    std::shared_ptr<context> _context;
 };
 
 } // namespace kaonic::grpc

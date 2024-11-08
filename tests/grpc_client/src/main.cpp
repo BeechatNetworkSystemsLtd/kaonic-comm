43e8a900b4fc2a60ff506d587059bfbb08c5f470
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2024-11-08 17:10:40 +0200

    feat: add grpc_client RecieveStream testing

--- tests/grpc_client/src/main.cpp
+++ tests/grpc_client/src/main.cpp
@@ -18,12 +18,24 @@ auto main(int argc, char** argv) noexcept -> int {
     auto radio_stub = Radio::NewStub(grpc_channel);
     log::info("[Client] Client started on {} IP", server_ip);
 
-    InfoResponse info {};
+    InfoResponse info;
     grpc::ClientContext info_context;
     if (auto err = device_stub->GetInfo(&info_context, Empty(), &info); !err.ok()) {
         log::info("[Client] Unable to get info: {}", err.error_message());
         return -1;
     }
 
+    grpc::ClientContext recieve_context;
+    auto reciever = radio_stub->ReceiveStream(&recieve_context, ReceiveRequest {});
+    ReceiveResponse response;
+    while (reciever->Read(&response)) {
+        const auto& frame = response.frame();
+        log::info("[Client] Recieved frame length: {}", frame.length());
+        log::info("[Client] Recieved frame crc32: {}", frame.crc32());
+        log::info("[Client] Recieved frame buffer elements: ");
+        for (const auto& data : frame.data()) {
+            log::info("[Client] {}", data);
+        }
+    }
     return 0;
 }

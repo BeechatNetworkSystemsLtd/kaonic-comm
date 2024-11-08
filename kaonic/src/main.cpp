f2b13cd791f93525670f96eef49d41d949f342ee
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2024-11-08 15:50:02 +0200

    feat: update server main

--- kaonic/src/main.cpp
+++ kaonic/src/main.cpp
@@ -18,19 +18,19 @@ auto main(int argc, char** argv) noexcept -> int {
     log::info("[Server] Start comm server with FW version {}", VERSION);
 
     std::unique_ptr<comm::radio> radio = std::make_unique<comm::rf215_radio>();
-    if (auto err = radio->open(); !err.is_ok()) {
+    if (auto err = radio->open(comm::radio_config {}); !err.is_ok()) {
         log::error("[Server] Unable to open radio device: {}", err.to_str());
         return -1;
     }
 
     auto context = std::make_shared<kaonic::grpc::context>(std::move(radio), VERSION);
-    kaonic::grpc::device_service device_serv(context);
-    kaonic::grpc::radio_service radio_serv(context);
+    kaonic::grpc::device_service device_svc { context };
+    kaonic::grpc::radio_service radio_svc { context };
 
     ::grpc::ServerBuilder builder;
     builder.AddListeningPort("0.0.0.0:50051", ::grpc::InsecureServerCredentials());
-    builder.RegisterService(&device_serv);
-    builder.RegisterService(&radio_serv);
+    builder.RegisterService(&device_svc);
+    builder.RegisterService(&radio_svc);
 
     std::unique_ptr<::grpc::Server> server(builder.BuildAndStart());
     log::info("[Server] Server is listening on port 50051");

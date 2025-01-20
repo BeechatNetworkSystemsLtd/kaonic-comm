d66f6235b6ac725453620819b9d783cfbdcd0b7a
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2025-01-20 14:26:23 +0200

    feat: add Network Receiver class

---
+++ kaonic/src/comm/mesh/network_receiver.cpp
@@ -0,0 +1,25 @@
+#include "kaonic/comm/mesh/network_receiver.hpp"
+
+#include "kaonic/common/logging.hpp"
+
+namespace kaonic::comm::mesh {
+
+auto network_broadcast_receiver::attach_listener(
+    const std::shared_ptr<network_receiver>& listener) noexcept -> void {
+    _listeners.push_back(listener);
+}
+
+auto network_broadcast_receiver::on_receive(const frame& frame) -> void {
+    for (auto& listener_ptr : _listeners) {
+        auto listener = listener_ptr.lock();
+
+        if (!listener) {
+            log::error("[Radio Broadcaster] Listener is not initialized");
+            return;
+        }
+
+        listener->on_receive(frame);
+    }
+}
+
+} // namespace kaonic::comm::mesh

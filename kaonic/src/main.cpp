748b37bfa9969778ce93c4c74e1f81fa05028cc5
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2025-01-20 23:46:10 +0200

    chore: add dedicated function for rf215 creating

--- kaonic/src/main.cpp
+++ kaonic/src/main.cpp
@@ -19,6 +19,49 @@
 using namespace kaonic;
 using namespace std::chrono_literals;
 
+static auto create_rf215(const drivers::spi_config& spi_config,
+                         gpiod::line&& rst_line,
+                         gpiod::line&& irq_line) -> std::shared_ptr<comm::rf215_radio> {
+    auto spi = std::make_unique<drivers::spi>();
+    if (auto err = spi->open(spi_config); !err.is_ok()) {
+        log::info("[Server] Unable to open SPI driver {}", spi_config.device_path);
+        return nullptr;
+    }
+
+    comm::radio_context radio_context;
+    radio_context.spi = std::move(spi);
+
+    if (!rst_line) {
+        log::error("[Server] Unable to get RST line for SPI {}", spi_config.device_path);
+        return nullptr;
+    }
+
+    if (!irq_line) {
+        log::error("[Server] Unable to get IRQ line for SPI {}", spi_config.device_path);
+        return nullptr;
+    }
+
+    rst_line.request({ "kaonic", gpiod::line_request::DIRECTION_OUTPUT, 0 });
+    irq_line.request({ "kaonic", gpiod::line_request::DIRECTION_INPUT, 0 });
+
+    if (!rst_line.is_requested()) {
+        log::error("[Server] Unable to request RST line on output for SPI {}",
+                   spi_config.device_path);
+        return nullptr;
+    }
+
+    if (!irq_line.is_requested()) {
+        log::error("[Server] Unable to request IRQ line A on input for SPI {}",
+                   spi_config.device_path);
+        return nullptr;
+    }
+
+    radio_context.reset_line = std::make_unique<gpiod::line>(std::move(rst_line));
+    radio_context.handle_line = std::make_unique<gpiod::line>(std::move(irq_line));
+
+    return std::make_shared<comm::rf215_radio>(std::move(radio_context));
+}
+
 auto main(int argc, char** argv) noexcept -> int {
     log::info("[Server] Start comm server with FW version {}", kaonic::info::version);
 
@@ -35,24 +78,6 @@ auto main(int argc, char** argv) noexcept -> int {
         .mode = kaonic::config::rfb_spi.mode,
         .bits_per_word = kaonic::config::rfb_spi.bpw,
     };
-
-    auto spi_a = std::make_unique<drivers::spi>();
-    if (auto err = spi_a->open(spi_a_config); !err.is_ok()) {
-        log::info("[Server] Unable to open SPI A driver");
-        return -1;
-    }
-
-    auto spi_b = std::make_unique<drivers::spi>();
-    if (auto err = spi_b->open(spi_b_config); !err.is_ok()) {
-        log::info("[Server] Unable to open SPI B driver");
-        return -1;
-    }
-
-    comm::radio_context radio_context_a;
-    radio_context_a.spi = std::move(spi_a);
-
-    comm::radio_context radio_context_b;
-    radio_context_b.spi = std::move(spi_b);
 
     auto gpio_a_rst_chip = gpiod::chip(std::string { kaonic::config::rfa_gpio.rst_chip_name },
                                        gpiod::chip::OPEN_BY_NAME);
@@ -72,65 +97,18 @@ auto main(int argc, char** argv) noexcept -> int {
     auto irq_line_a = gpio_a_irq_chip.get_line(kaonic::config::rfa_gpio.irq_chip_line);
     auto irq_line_b = gpio_b_irq_chip.get_line(kaonic::config::rfb_gpio.irq_chip_line);
 
-    if (!rst_line_a) {
-        log::error("[Server] Unable to get rst line A {}", kaonic::config::rfa_gpio.rst_chip_line);
+    auto radio_a = create_rf215(spi_a_config, std::move(rst_line_a), std::move(irq_line_a));
+    auto radio_b = create_rf215(spi_b_config, std::move(rst_line_b), std::move(irq_line_b));
+
+    if (!radio_a || !radio_a) {
         return -1;
     }
 
-    if (!rst_line_b) {
-        log::error("[Server] Unable to get rst line B {}", kaonic::config::rfb_gpio.rst_chip_line);
-        return -1;
-    }
-
-    if (!irq_line_a) {
-        log::error("[Server] Unable to get IRQ line A {}", kaonic::config::rfa_gpio.irq_chip_line);
-        return -1;
-    }
-
-    if (!irq_line_b) {
-        log::error("[Server] Unable to get IRQ line B {}", kaonic::config::rfb_gpio.irq_chip_line);
-        return -1;
-    }
-
-    rst_line_a.request({ "kaonic", gpiod::line_request::DIRECTION_OUTPUT, 0 });
-    rst_line_b.request({ "kaonic", gpiod::line_request::DIRECTION_OUTPUT, 0 });
-
-    irq_line_a.request({ "kaonic", gpiod::line_request::DIRECTION_INPUT, 0 });
-    irq_line_b.request({ "kaonic", gpiod::line_request::DIRECTION_INPUT, 0 });
-
-    if (!rst_line_a.is_requested()) {
-        log::error("[Server] Unable to request reset line A on output");
-        return -1;
-    }
-
-    if (!rst_line_b.is_requested()) {
-        log::error("[Server] Unable to request reset line B on output");
-        return -1;
-    }
-
-    if (!irq_line_a.is_requested()) {
-        log::error("[Server] Unable to request IRQ line A on input");
-        return -1;
-    }
-
-    if (!irq_line_b.is_requested()) {
-        log::error("[Server] Unable to request IRQ line B on input");
-        return -1;
-    }
-
-    radio_context_a.reset_line = std::make_unique<gpiod::line>(std::move(rst_line_a));
-    radio_context_b.handle_line = std::make_unique<gpiod::line>(std::move(rst_line_b));
-
-    radio_context_a.reset_line = std::make_unique<gpiod::line>(std::move(irq_line_a));
-    radio_context_b.handle_line = std::make_unique<gpiod::line>(std::move(irq_line_b));
-
-    auto radio_a = std::make_shared<comm::rf215_radio>(std::move(radio_context_a));
     if (auto err = radio_a->init(); !err.is_ok()) {
         log::error("[Server] Unable to init the radio 1");
         return -1;
     }
 
-    auto radio_b = std::make_shared<comm::rf215_radio>(std::move(radio_context_b));
     if (auto err = radio_b->init(); !err.is_ok()) {
         log::error("[Server] Unable to init the radio 2");
         return -1;

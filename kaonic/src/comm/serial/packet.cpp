be6346e75ecc45b7cc8cca5d8db64f8b545f3c05
Yurii Vavrynchuk <yurii.vavrynchuk@embrox.com>
2025-01-21 13:51:41 +0200

    feat: add packet header to the serial packet

--- kaonic/src/comm/serial/packet.cpp
+++ kaonic/src/comm/serial/packet.cpp
@@ -4,6 +4,8 @@
 
 namespace kaonic::comm::serial {
 
+constexpr static uint16_t magic = 0x22;
+
 enum class packet_type : uint16_t {
     unknown = 0,
     config = 1,
@@ -11,16 +13,31 @@ enum class packet_type : uint16_t {
     receive = 3,
 };
 
+struct packet_header final {
+    uint16_t magic;
+    packet_type type;
+    uint8_t reserved[16];
+};
+
 auto packet::decode(const buffer_t& buffer, payload_t& payload) noexcept -> void {
-    if (buffer.size() < 2) {
+    if (buffer.size() < sizeof(packet_header)) {
         payload = error_payload {};
         return;
     }
 
-    switch (buffer[0]) {
-        case static_cast<uint8_t>(packet_type::config): {
+    packet_header header;
+    memcpy(&header, buffer.data(), sizeof(header));
+
+    if (header.magic != magic) {
+        payload = error_payload {};
+        return;
+    }
+
+    switch (header.type) {
+        case packet_type::config: {
             ConfigurationRequest config;
-            if (!config.ParseFromArray(buffer.data() + 1, buffer.size() - 1)) {
+            if (!config.ParseFromArray(buffer.data() + sizeof(header),
+                                       buffer.size() - sizeof(header))) {
                 log::warn("[Serial Service] TX failed: unable to parse config packet");
                 payload = error_payload {};
                 break;
@@ -28,9 +45,10 @@ auto packet::decode(const buffer_t& buffer, payload_t& payload) noexcept -> void
             payload = config;
             break;
         }
-        case static_cast<uint8_t>(packet_type::transmit): {
+        case packet_type::transmit: {
             TransmitRequest request;
-            if (!request.ParseFromArray(buffer.data() + 1, buffer.size() - 1)) {
+            if (!request.ParseFromArray(buffer.data() + sizeof(header),
+                                        buffer.size() - sizeof(header))) {
                 log::warn("[Serial Service] TX failed: unable to parse frame packet");
                 payload = error_payload {};
                 break;
@@ -38,9 +56,10 @@ auto packet::decode(const buffer_t& buffer, payload_t& payload) noexcept -> void
             payload = request;
             break;
         }
-        case static_cast<uint8_t>(packet_type::receive): {
+        case packet_type::receive: {
             ReceiveResponse response;
-            if (!response.ParseFromArray(buffer.data() + 1, buffer.size() - 1)) {
+            if (!response.ParseFromArray(buffer.data() + sizeof(header),
+                                         buffer.size() - sizeof(header))) {
                 log::warn("[Serial Service] RX failed: unable to parse frame packet");
                 payload = error_payload {};
                 break;
@@ -60,28 +79,36 @@ auto serial::packet::encode(const payload_t& payload, buffer_t& buffer) noexcept
         [&buffer](auto&& payload) {
             using T = std::decay_t<decltype(payload)>;
 
+            packet_header header;
+            header.magic = magic;
+
             size_t size = 0;
             buffer.clear();
             if constexpr (std::is_same_v<T, ConfigurationRequest>) {
+                header.type = packet_type::config;
                 size = payload.ByteSizeLong();
-                buffer.resize(size + 1);
-                buffer[0] = static_cast<uint8_t>(packet_type::config);
-                payload.SerializeToArray(buffer.data() + 1, static_cast<int>(size));
+                buffer.resize(size + sizeof(header));
+                memcpy(buffer.data(), &header, sizeof(header));
+                payload.SerializeToArray(buffer.data() + sizeof(header), static_cast<int>(size));
             }
             if constexpr (std::is_same_v<T, TransmitRequest>) {
+                header.type = packet_type::transmit;
                 size = payload.ByteSizeLong();
-                buffer.resize(size + 1);
-                buffer[0] = static_cast<uint8_t>(packet_type::transmit);
-                payload.SerializeToArray(buffer.data() + 1, static_cast<int>(size));
+                buffer.resize(size + sizeof(header));
+                memcpy(buffer.data(), &header, sizeof(header));
+                payload.SerializeToArray(buffer.data() + sizeof(header), static_cast<int>(size));
             }
             if constexpr (std::is_same_v<T, ReceiveResponse>) {
+                header.type = packet_type::receive;
                 size = payload.ByteSizeLong();
-                buffer.resize(size + 1);
-                buffer[0] = static_cast<uint8_t>(packet_type::receive);
-                payload.SerializeToArray(buffer.data() + 1, static_cast<int>(size));
+                buffer.resize(size + sizeof(header));
+                memcpy(buffer.data(), &header, sizeof(header));
+                payload.SerializeToArray(buffer.data() + sizeof(header), static_cast<int>(size));
             }
             if constexpr (std::is_same_v<T, error_payload>) {
-                buffer.push_back(static_cast<uint8_t>(packet_type::unknown));
+                header.type = packet_type::unknown;
+                buffer.resize(sizeof(header));
+                memcpy(buffer.data(), &header, sizeof(header));
             }
         },
         payload);

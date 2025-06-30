b37f7fb352d5f31f048903d47767d319a922c848
Maksym Ostapenko <maksym.ostapenko@embrox.com>
2025-06-30 15:54:01 +0300

    feat: add support for fsk modulation settings

--- kaonic/include/kaonic/comm/radio/radio.hpp
+++ kaonic/include/kaonic/comm/radio/radio.hpp
@@ -21,7 +21,82 @@ struct radio_phy_config_ofdm {
     uint32_t opt = 0;
 };
 
-using radio_phy_config_t = std::variant<radio_phy_config_ofdm>;
+struct radio_phy_config_fsk {
+    // FSK Bandwidth Time Product
+    uint8_t bt = 0x03;
+    // FSK Modulation Index Scale
+    uint8_t midxs = 0x01;
+    // FSK Modulation Index
+    uint8_t midx = 0x03;
+    // FSK Modulation Order
+    uint8_t mord = 0x00;
+
+    uint16_t preamble_length = 0;
+
+    bool freq_inversion = false;
+
+    // MR-FSK Symbol Rate
+    uint8_t srate = 0x00;
+
+    // Preamble Detection Mode
+    uint8_t pdtm = 0x00;
+
+    // Receiver Override
+    uint8_t rxo = 0x02;
+
+    // Receiver Preamble Time Out
+    uint8_t rxpto = 0x00;
+
+    // Mode Switch Enable
+    uint8_t mse = 0x00;
+
+    bool preamble_inversion = false;
+
+    // FEC Scheme
+    uint8_t fecs = 0x00;
+
+    // FEC Interleaving Enable
+    bool fecie = true;
+
+    // SFD Detection Threshold
+    uint8_t sfdt = 0x08;
+
+    // Preamble Detection Threshold
+    uint8_t pdt = 0x05;
+
+    // SFD Quantization
+    bool sftq = false;
+
+    // SFD 32Bit
+    uint8_t sfd32 = 0;
+
+    bool rawbit = true;
+
+    // Configuration of PPDU with SFD1
+    uint8_t csfd1 = 0x02;
+
+    // Configuration of PPDU with SFD0
+    uint8_t csfd0 = 0x00;
+
+    uint16_t sfd0 = 0x7209;
+    uint16_t sfd1 = 0x72F6;
+
+    uint8_t sfd = 0x00;
+
+    // Data Whitening
+    uint8_t dw = 0x01;
+
+    // FSK Preemphasis
+    bool pe = false;
+    // FSK Direct Modulation Enable
+    bool en = false;
+
+    uint8_t fskpe0 = 0x00;
+    uint8_t fskpe1 = 0x00;
+    uint8_t fskpe2 = 0x00;
+};
+
+using radio_phy_config_t = std::variant<radio_phy_config_ofdm, radio_phy_config_fsk>;
 
 struct radio_config final {
     uint32_t freq = 869535;
